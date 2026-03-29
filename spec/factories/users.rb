9a8114be5d130abc86be5c694714883461121917
kishihara3q-debug <k.ishihara3q@gmail.com>
2026-03-29 16:09:37 +0900

    FakerでUNIQUE再現するため修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname { 'takotakosan' }
-    email { 'test@example.com' }
+    email { Faker::Internet.email }
     password { 'abc123' }
     password_confirmation { password }
 
