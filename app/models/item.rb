1ccb779fa68f159c53db39403fa03fcef1e0bee0
kishihara3q-debug <k.ishihara3q@gmail.com>
2026-04-02 08:01:06 +0900

    ActiveHash関連の追加修正
    Itemモデルと選択式の関連づけと一部修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,10 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :price, presence: true
+  validates :price,
+            presence: true,
+            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :user, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
