7b87f536dc19cda1e6f9ff2cb0518a696bacebfe
kishihara3q-debug <k.ishihara3q@gmail.com>
2026-04-04 08:19:22 +0900

    Active Storageの導入
    ・テーブル紐付け
    ・ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,20 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image,
+      :name,
+      :description,
+      :category_id,
+      :condition_id,
+      :shipping_fee_id,
+      :prefecture_id,
+      :shipping_day_id,
+      :price
+    )
+  end
 end
