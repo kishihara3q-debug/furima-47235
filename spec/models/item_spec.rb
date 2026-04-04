79f08cc4b8a31248e571160f71fea47d17075c9f
kishihara3q-debug <k.ishihara3q@gmail.com>
2026-04-04 22:10:35 +0900

    userバリデーション戻しと修正
    削除ではなく、エラーメッセージが間違っていたため復元➕修正しました。

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,6 +67,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
+      it 'userが空だと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
       it 'imageが空だと登録できない' do
         @item.image = nil
         @item.valid?
