0575cf53e978321736edb0933f2e1e09bf91ec4f
kishihara3q-debug <k.ishihara3q@gmail.com>
2026-04-04 09:58:22 +0900

    出品登録関連のViewのパス修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
   end
 
@@ -7,6 +9,12 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
