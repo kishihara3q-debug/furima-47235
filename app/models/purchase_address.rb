cb424363535e30899313aad8ead2fedc024da0d6
kishihara3q-debug <k.ishihara3q@gmail.com>
2026-04-11 20:53:56 +0900

    purchase_address.rb　バリデーション修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -8,7 +8,6 @@ class PurchaseAddress
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: '3桁ハイフン4桁で半角文字列で入力してください' }
     validates :city
     validates :address_line
-    validates :building_name
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: '10~11桁の数字で半角文字列(ハイフンなし）で入力してください' }
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
