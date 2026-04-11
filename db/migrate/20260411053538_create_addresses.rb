65afe69e73e20ed02fe8e22c4674b3fdf59323f7
kishihara3q-debug <k.ishihara3q@gmail.com>
2026-04-11 15:08:26 +0900

    addressesテーブル記述修正

--- db/migrate/20260411053538_create_addresses.rb
+++ db/migrate/20260411053538_create_addresses.rb
@@ -5,7 +5,7 @@ class CreateAddresses < ActiveRecord::Migration[7.1]
       t.integer :prefecture_id,    null: false
       t.string  :city,             null: false
       t.string  :address_line,     null: false
-      t.string  :building_name,
+      t.string  :building_name
       t.string  :phone_number,     null: false
       t.references :purchase,      null: false, foreign_key: true
 
