d8dea8a8e9ad7448bf970ba4c6ad8f0fb3eb23a0
kishihara3q-debug <k.ishihara3q@gmail.com>
2026-04-11 17:21:32 +0900

    PurchaseAddressのデータをテーブルに保存する処理

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -12,4 +12,16 @@ class PurchaseAddress
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: '10~11桁の数字で半角文字列(ハイフンなし）で入力してください' }
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+
+    Address.create(postal_code: postal_code,
+                   prefecture_id: prefecture_id,
+                   city: city,
+                   address_line: address_line
+                   building_name: building_name
+                   phone_number: phone_number
+                   purchase_id: purchase.id)
+  end
 end
