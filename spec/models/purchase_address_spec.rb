d2986ba4c61cefcb88e8c7dc8b94c5061f1c0a01
kishihara3q-debug <k.ishihara3q@gmail.com>
2026-04-12 21:31:28 +0900

    purchase_address_specの更新

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -18,5 +18,78 @@ RSpec.describe PurchaseAddress, type: :model do
         expect(@purchase_address).to be_valid
       end
     end
+    context '内容に問題がある場合' do
+      it '郵便番号が空の場合に保存できないこと' do
+        @purchase_address.postal_code = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+
+      it '郵便番号が「3桁ハイフン4桁」の形式でないと保存できないこと' do
+        @purchase_address.postal_code = '1234567'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include('Postal code 3桁ハイフン4桁で半角文字列で入力してください')
+      end
+
+      it '郵便番号に全角が含まれていると保存できないこと' do
+        @purchase_address.postal_code = '１２３-４５６７'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include('Postal code 3桁ハイフン4桁で半角文字列で入力してください')
+      end
+
+      it '都道府県が---の場合に保存できないこと' do
+        @purchase_address.prefecture_id = 0
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '市区町村が空の場合に保存できないこと' do
+        @purchase_address.city = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("City can't be blank")
+      end
+
+      it '番地が空の場合に保存できないこと' do
+        @purchase_address.address_line = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Address line can't be blank")
+      end
+
+      it '電話番号が空の場合に保存できないこと' do
+        @purchase_address.phone_number = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it '電話番号が「10~11桁の数字」の形式でないと保存できないこと' do
+        @purchase_address.phone_number = '1234567890123'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include('Phone number 10~11桁の数字で半角文字列(ハイフンなし）で入力してください')
+      end
+
+      it '電話番号に全角が含まれていると保存できないこと' do
+        @purchase_address.phone_number = '１２３４５６７８９０１２３'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include('Phone number 10~11桁の数字で半角文字列(ハイフンなし）で入力してください')
+      end
+
+      it '電話番号にハイフンが含まれていると保存できないこと' do
+        @purchase_address.phone_number = '090-1234-5678'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include('Phone number 10~11桁の数字で半角文字列(ハイフンなし）で入力してください')
+      end
+
+      it 'user_idが空の場合に保存できないこと' do
+        @purchase_address.user_id = nil
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'item_idが空の場合に保存できないこと' do
+        @purchase_address.item_id = nil
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Item can't be blank")
+      end
+    end
   end
 end
